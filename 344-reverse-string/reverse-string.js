62884b2801dd2edadbd27fbe755e72617f51d569
Tanu Kashyap <tanukashyap760@gmail.com>
2026-01-26 13:53:33 +0530

    Time: 3 ms (15.71%) | Memory: 59.6 MB (92.63%) - LeetSync

---
+++ 344-reverse-string/reverse-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let n=s.length;
+    for(let i=0;i<n/2;i++){
+        let temp=s[i];
+        s[i]=s[n-(i+1)];
+        s[n-(i+1)]=temp; 
+    }
+    return s;
+    
+};
