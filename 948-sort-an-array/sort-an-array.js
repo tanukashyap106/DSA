0d858ae1c3b873e7d28d18ac4894dec622004b01
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-01-29 13:53:11 +0530

    Time: 151 ms (23.74%) | Memory: 82.9 MB (17.88%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    if(nums.length<=1) return nums;
+    let m=nums.length/2;
+    let left=sortArray(nums.slice(0,m));
+    let right=sortArray(nums.slice(m));
+    return merge(left,right);
+    
+};
+function merge(left,right){
+    let result=[];
+    let i=0;
+    let j=0;
+    while(i<left.length && j<right.length){
+        if(left[i]<right[j]){
+           result.push(left[i]);
+           i++;
+        }
+        else{
+            result.push(right[j]);
+            j++;
+        }
+    }
+    return [...result,...left.slice(i),...right.slice(j)];
+}
