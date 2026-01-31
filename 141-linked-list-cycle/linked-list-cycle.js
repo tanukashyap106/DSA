1a659f4c2274fdbef0e95504b4aea93e7481f87c
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-01-31 12:31:27 +0530

    Time: 44 ms (94.67%) | Memory: 59.3 MB (54.73%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,15 +11,17 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-let Node=new Set();
-let curr=head;
-while(curr!==null){
-    if(Node.has(curr)){
-        return true;
+    if(head===null) return false; 
+    let slow=head;
+    let fast=head.next;
+    while(slow!==fast){
+    if(fast===null || fast.next===null){
+        return false;
+        }
+       slow=slow.next;
+       fast=fast.next.next;
+    
     }
-    Node.add(curr);
-    curr=curr.next;
-}
-return false;
+        return true;
     
 };
