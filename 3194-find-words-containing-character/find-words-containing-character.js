d060ae22f427fd4b25b43e3c0705ee737e3c3fef
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-02-02 14:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 59.5 MB (6.18%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -4,13 +4,15 @@
  * @return {number[]}
  */
 var findWordsContaining = function(words, x) {
-    let result=[];
+    let res=[];
     for(let i=0;i<words.length;i++){
         for(let j=0;j<words[i].length;j++){
-        if(words[i][j]===x){
-            result.push(i);
-            break;
+            if(words[i][j]===x){
+                res.push(i);
+                break;
+            }
         }
-    }}
-    return result;
+    }
+    return res;
+    
 };
