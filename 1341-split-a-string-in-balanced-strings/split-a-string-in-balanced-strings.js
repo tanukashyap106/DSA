0e298aabb6f3e22e7909476cbeba85502430218f
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-02-02 22:25:45 +0530

    Time: 1 ms (25.58%) | Memory: 53 MB (96.15%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-    let r=0;
-    let l=0;
-    let count=0
+    let temp=0;
+    let count=0;
     for(let i=0;i<s.length;i++){
         if(s[i]==="R"){
-            ++r;
+            ++temp;
         }
         else{
-            ++l;
+            --temp;
         }
 
-        if(r===l){
+        if(temp===0){
             count++;
-            r=0;
-            l=0;
+
         }
     }
     return count;
