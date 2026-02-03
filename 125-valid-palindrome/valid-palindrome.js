0d04dc23b64daa9248b6f71d4ec3dbcd0d26dc73
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-02-03 14:06:10 +0530

    Time: 15 ms (8.63%) | Memory: 63.8 MB (8.68%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s=s.toLowerCase();
+    let filteredString="";
+    let rev="";
+    for(let i=0;i<s.length;i++){
+        if(s[i].match(/[a-z0-9]/i)){
+            filteredString+=s[i];
+            rev=s[i]+rev;
+        }
+    }
+    return filteredString===rev;
+};
