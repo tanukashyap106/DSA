3e224913f4e7b85fa2913ef0aab46c5819653208
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-02-13 22:34:40 +0530

    Time: 0 ms (100.00%) | Memory: 80.1 MB (88.12%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l=0;
+        int r=arr.length-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(arr[m+1]>arr[m]){
+                l=m+1;
+            }
+            else{
+                r=m;
+            }
+        }
+        return l;
+        
+    }
+}
