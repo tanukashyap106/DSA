a8e9941c80de96e9d8a741466229e5596cf862b7
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-02-21 20:06:59 +0530

    Time: 1 ms (60.38%) | Memory: 49.2 MB (9.39%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -3,18 +3,22 @@ class Solution {
         int n= arr.length;
         int[] maxL=new int[n];
         maxL[0]=arr[0];
+        int[] maxR=new int[n];
+        maxR[n-1]=arr[n-1];
         for(int i=1;i<n;i++){
             maxL[i]=Math.max(maxL[i-1],arr[i]);
+            maxR[n-1-i]=Math.max(arr[n-1-i],maxR[n-i]);
+
         }
-        int[] maxR=new int[n];
+       /* int[] maxR=new int[n];
         maxR[n-1]=arr[n-1];
         for(int i=n-2;i>=0;i--){
             maxR[i]=Math.max(arr[i],maxR[i+1]);
-        }
+        }*/
         int ans=0;
         for(int i=0;i<n;i++){
-            int waterTrapped=(Math.min(maxL[i],maxR[i])-arr[i]);
-            ans +=Math.max(waterTrapped,0);
+           // int waterTrapped=(Math.min(maxL[i],maxR[i])-arr[i]);
+            ans +=Math.max(Math.min(maxL[i],maxR[i])-arr[i],0);
         }
         return ans;
         
