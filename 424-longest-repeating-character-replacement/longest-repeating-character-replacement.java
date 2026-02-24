ce9583694744fd79669dd27877d8f8e17c81bdb2
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-02-24 21:40:23 +0530

    Time: 19 ms (35.67%) | Memory: 46.1 MB (79.85%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,27 +1,30 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        if (s == null || s.length() == 0) return 0;   
-        int[] map = new int[26];
-        int i = 0, maxWindow = 0;
-        for (int j = 0; j < s.length(); j++) {
-            map[s.charAt(j) - 'A']++;
-            while (!isWindowValid(map, k)) {
-                map[s.charAt(i) - 'A']--;
+        if(s==null || s.length()==0) return 0;
+        int[] map=new int[26];
+        int i=0,maxw=0;
+        for(int j=0;j<s.length();j++){
+            map[s.charAt(j)-'A']++;
+            while(!isValid(map,k)){
+                map[s.charAt(i)-'A']--;
                 i++;
             }
-            maxWindow = Math.max(maxWindow, j - i + 1);
-        }
-        return maxWindow;
-    }
+            maxw=Math.max(maxw,j-i+1);
 
-    private boolean isWindowValid(int[] map, int k) {
-        int totalCount = 0, maxCount = 0;
-        for (int count : map) {
-            totalCount += count;
-            maxCount = Math.max(maxCount, count);
         }
-        return (totalCount - maxCount <= k);
+        return maxw;
+    }
+        private boolean isValid(int[] map,int k){
+            int totalCount=0;
+            int maxCount=0;
+            for(int count : map){
+                totalCount+=count;
+                maxCount=Math.max(maxCount,count);
+
+            }     
+
+            return (totalCount-maxCount<=k);
+        
+        }
     
-
-    }
 }
