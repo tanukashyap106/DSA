e440b9e31f5d1a451580d0d6ce901af35e65fa3a
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-02-25 20:15:02 +0530

    Time: 33 ms (31.23%) | Memory: 156.1 MB (13.25%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,24 +1,24 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] arr, int k) {
-        List<Integer> res = new ArrayList<>();
-        Deque<Integer> q = new LinkedList<>();
-        int i = 0, j = 0;
-        while (j < arr.length) {
-            while (!q.isEmpty() && arr[j] > q.peekLast()) {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> res=new ArrayList<>();
+        Deque<Integer> q=new LinkedList<>();
+        int i=0,j=0;
+        while(j<nums.length){
+            while(!q.isEmpty() && nums[j]>q.peekLast()){
                 q.pollLast();
             }
-            q.offerLast(arr[j]);
-
-            if (j >= k - 1) {
+            q.offerLast(nums[j]);
+            if(j>=k-1){
                 res.add(q.peekFirst());
-                if (arr[i] == q.peekFirst()) q.pollFirst();
+                if(nums[i]==q.peekFirst())
+                q.pollFirst();
                 i++;
             }
             j++;
         }
-        int[] ans = new int[res.size()];
-        for (int x = 0; x < res.size(); x++) {
-            ans[x] = res.get(x);
+        int[] ans=new int[res.size()];
+        for(int x=0;x<ans.length;x++){
+            ans[x]=res.get(x);
         }
         return ans;
     }
