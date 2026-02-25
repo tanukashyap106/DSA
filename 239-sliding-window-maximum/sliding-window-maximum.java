a0227f83f75865852e69b574c28dc265a85a394b
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-02-25 19:56:58 +0530

    Time: 36 ms (21.48%) | Memory: 156 MB (18.41%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,46 +1,25 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0) return new int[0];
-        int n = nums.length;
-        int[] res = new int[n - k + 1]; 
-        int r = 0; 
-        Deque<Integer> q = new ArrayDeque<>();
-        
-        for (int j = 0; j < n; j++) {
-
-            if (!q.isEmpty() && q.peekFirst() < j - k + 1) {
-                q.pollFirst();
-            }
-            while (!q.isEmpty() && nums[q.peekLast()] < nums[j]) {
-                q.pollLast();
-            }
-            q.offerLast(j);
-            if (j >= k - 1) {
-                res[r++] = nums[q.peekFirst()];
-            }
-        }
-        
-        return res;
-    }
-}
-     
-     /*
-        List res = new ArrayList<>();
-        Deque q = new LinkedList<>();
+    public int[] maxSlidingWindow(int[] arr, int k) {
+        List<Integer> res = new ArrayList<>();
+        Deque<Integer> q = new LinkedList<>();
         int i = 0, j = 0;
-        while (j < nums.length) {
-            while (!q.isEmpty() && nums[j] > q.peekLast()) {
+        while (j < arr.length) {
+            while (!q.isEmpty() && arr[j] > q.peekLast()) {
                 q.pollLast();
             }
-            q.offerLast(nums[j]);
+            q.offerLast(arr[j]);
 
             if (j >= k - 1) {
                 res.add(q.peekFirst());
-                if (nums[i] == q.peekFirst()) q.pollFirst();
+                if (arr[i] == q.peekFirst()) q.pollFirst();
                 i++;
             }
             j++;
         }
-        return res;
+        int[] ans = new int[res.size()];
+        for (int x = 0; x < res.size(); x++) {
+            ans[x] = res.get(x);
+        }
+        return ans;
     }
-}*/
+}
