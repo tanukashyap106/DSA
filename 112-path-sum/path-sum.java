f1a1206fe0b29aaf5ed5a56865d3711f626bae77
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-03-02 20:00:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (93.07%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean ans=false;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null) return false;
+        traverse(root,0,targetSum);
+        return ans;
+        
+    }
+    private void traverse(TreeNode curr,int currSum,int targetSum){
+        int newSum=currSum+curr.val;
+        if(curr.left==null && curr.right==null){
+            if(newSum==targetSum){
+                ans= true;
+            }
+        }
+        if(curr.left!=null) traverse(curr.left,newSum,targetSum);
+        if(curr.right!=null) traverse(curr.right,newSum,targetSum);
+    }
+}
