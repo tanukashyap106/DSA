e0e62016ea89789dbb96bc5291509b8d0e558e2b
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-03-02 20:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (96.65%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -14,21 +14,15 @@
  * }
  */
 class Solution {
-    boolean ans=false;
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null) return false;
-        traverse(root,0,targetSum);
-        return ans;
-        
-    }
-    private void traverse(TreeNode curr,int currSum,int targetSum){
-        int newSum=currSum+curr.val;
-        if(curr.left==null && curr.right==null){
-            if(newSum==targetSum){
-                ans=true;
+        if(root.left==null && root.right==null){
+            if(root.val==targetSum){
+                return true;
             }
         }
-        if(curr.left!=null) traverse(curr.left,newSum,targetSum);
-        if(curr.right!=null) traverse(curr.right,newSum,targetSum);
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+
+        
     }
 }
