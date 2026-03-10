96f877d29268117d07d43cdf030a6b23943add81
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-03-10 22:09:38 +0530

    Time: 8 ms (21.94%) | Memory: 46.5 MB (82.62%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,20 +9,22 @@
  */
 class Solution {
     TreeNode lca=null;
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
-        traversal(root, p, q);
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        traversal(root,p,q);
         return lca;
     }
-    private int traversal(TreeNode curr, TreeNode p, TreeNode q) {
-        if (curr == null) return 0;
-        int left = traversal(curr.left, p, q);
-        int right = traversal(curr.right, p, q);
-        int mid = (curr == p || curr == q) ? 1 : 0;
-
-        if (left + right + mid >= 2 && lca == null) {
-            lca = curr;
+    private int traversal(TreeNode curr,TreeNode p,TreeNode q){
+        if(curr==null) return 0;
+        int count=0;
+        int left=traversal(curr.left,p,q);
+        int right=traversal(curr.right,p,q);
+        if(curr.val==p.val || curr.val==q.val){
+            count++;
         }
-      return left + right + mid;
+        count=count+left+right;
+        if(count==2 && lca==null){
+            lca=curr;
+        }
+        return count;
     }
 }
-    
