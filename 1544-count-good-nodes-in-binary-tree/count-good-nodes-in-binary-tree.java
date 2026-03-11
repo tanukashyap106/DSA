12db9b5831d87bf616a73a52cce5bea6cacb945b
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-03-11 20:03:22 +0530

    Time: 2 ms (93.89%) | Memory: 56.5 MB (94.84%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -14,17 +14,16 @@
  * }
  */
 class Solution {
-          int ans = 0;  
-    public void traversal(TreeNode curr, int maxSeenSoFar) {
-        if (curr == null) return;
-        if (curr.val >= maxSeenSoFar) ans++;
-        int currMax = Math.max(maxSeenSoFar, curr.val);
-        traversal(curr.left, currMax);
-        traversal(curr.right, currMax);
+    int ans=0;
+    public void traversal(TreeNode curr,int maxSeen){
+        if(curr==null) return;
+        if(curr.val>=maxSeen) ans++;
+        int currMax=Math.max(maxSeen,curr.val);
+        traversal(curr.left,currMax);
+        traversal(curr.right,currMax);
     }
     public int goodNodes(TreeNode root) {
-        traversal(root, Integer.MIN_VALUE);
+        traversal(root,Integer.MIN_VALUE);
         return ans;
-    
     }
 }
