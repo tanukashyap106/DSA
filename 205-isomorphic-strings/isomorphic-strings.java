3f8e0073fccd722833661d5c6928ff196327b22b
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-03-13 22:37:51 +0530

    Time: 17 ms (38.21%) | Memory: 44.1 MB (23.65%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> mapST = new HashMap<>();
+        HashMap<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+
+            if (mapST.containsKey(cs)) {
+                if (mapST.get(cs) != ct) {
+                    return false;
+                }
+            } else {
+                mapST.put(cs, ct);
+            }
+
+            if (mapTS.containsKey(ct)) {
+                if (mapTS.get(ct) != cs) {
+                    return false;
+                }
+            } else {
+                mapTS.put(ct, cs);
+            }
+        }
+
+        return true;
+    }
+}
