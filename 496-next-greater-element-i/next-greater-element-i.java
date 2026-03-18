2f2244867e9da256d57eec1b074bdf00e783d30b
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-03-18 22:07:27 +0530

    Time: 3 ms (92.60%) | Memory: 45.5 MB (30.83%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,32 +1,32 @@
 class Solution {
-    public int[] nextGreaterElement(int[] n1, int[] n2) {
-        Stack<Integer> s=new Stack<>();
-        Map<Integer,Integer>map=new HashMap<>();
-        int n=n2.length;
-        s.push(n2[n-1]);
-        map.put(n2[n-1],-1);
-        for(int i=n-2;i>=0;i--){
-            if(n2[i]<s.peek()){
-                map.put(n2[i],s.peek());
-            }
-            while(!s.isEmpty()){
-                if(s.peek()<n2[i]){
-                    s.pop();
-
-                }else{
-                    map.put(n2[i],s.peek());
-                    break;
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer,Integer> ngeMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        int n = nums2.length;
+        stack.push(nums2[n - 1]);
+        ngeMap.put(nums2[n - 1], -1);
+        for (int i = n - 2; i >= 0; i--) {
+            int top = stack.peek();
+            if (nums2[i] < top) {
+                ngeMap.put(nums2[i], top);
+            } else {
+                while (!stack.isEmpty()) {
+                    if (stack.peek() < nums2[i]) {
+                        stack.pop();
+                    } else {
+                        ngeMap.put(nums2[i], stack.peek());
+                        break;
+                    }
+                }
+                if (stack.isEmpty()) {
+                    ngeMap.put(nums2[i], -1);
                 }
             }
-            if(s.isEmpty()){
-                map.put(n2[i],-1);
-            }
-        
-        s.push(n2[i]);
+            stack.push(nums2[i]);
         }
-        int [] ans=new int[n1.length];
-        for(int i=0;i<n1.length;i++){
-           ans[i]= map.get(n1[i]);
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = ngeMap.get(nums1[i]);
         }
         return ans;
     }
