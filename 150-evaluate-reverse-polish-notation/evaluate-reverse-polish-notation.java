c03ddbebe56c4f865cee4465c65cf5e9f68171af
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-03-18 21:49:59 +0530

    Time: 9 ms (7.34%) | Memory: 45.6 MB (16.13%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,25 +1,23 @@
-//import.java.util.*;
-//import.java.function.*;
 class Solution {
-    public int evalRPN(String[] tokens) {
-        Stack<Integer> s=new Stack<>();
+    public int evalRPN(String[] arr) {
+
+        Stack<Integer> stack=new Stack<>();
         Map<String,BiFunction<Integer,Integer,Integer>> map=new HashMap<>();
-        map.put("+" ,(a,b)->b+a);
+        map.put("+",(a,b)->b+a);
         map.put("-",(a,b)->b-a);
         map.put("*",(a,b)->b*a);
         map.put("/",(a,b)->b/a);
-        for(int i=0;i<tokens.length;i++){
-            if(map.containsKey(tokens[i])){
-                int a=s.pop();
-                int b=s.pop();
-                int ans=map.get(tokens[i]).apply(a,b);
-                s.push(ans);
-            }
-            else{
-                s.push(Integer.parseInt(tokens[i]));
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                int a=stack.pop();
+                int b=stack.pop();
+                int ans=map.get(arr[i]).apply(a,b);
+                stack.push(ans);
+            }else{
+                stack.push(Integer.parseInt(arr[i]));
             }
         }
-    return s.pop();
+        return stack.pop();
         
     }
 }
