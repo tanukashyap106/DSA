e413fbbc24540f60a14115b58c6c41e33784b70a
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-03-22 20:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (48.02%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -14,24 +14,10 @@
  * }
  */
 class Solution {
-    TreeNode ans=null;
     public TreeNode searchBST(TreeNode root, int val) {
-        traversal(root,val);
-        return ans;
-    }
-    private void traversal(TreeNode curr,int val){
-        if(curr==null) return;
-        if(curr.val==val) 
-        {
-            ans=curr;
-            return;
-        }
-        else{
-            if(curr.val<val){
-               if(curr.right!=null) traversal(curr.right,val);
-            }else{
-                if(curr.left!=null) traversal(curr.left,val);
-            }
-        }
+        if (root == null || root.val==val)
+            return root;
+        return root.val > val ? searchBST(root.left, val) : searchBST(root.right, val);
+
     }
 }
