3ea6be55db17d5485d9349568a1c2e70282486b4
Tanu Kashyap <240127570+tanukashyap106@users.noreply.github.com>
2026-04-07 22:39:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (41.55%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.java
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.java
@@ -9,18 +9,14 @@
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        int l = 1;
-        int r = n;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            int res = guess(m);
-            if (res == 0)
-                return m;
-            else if (res < 0) {
-                r = m - 1;
-            } else {
-                l = m + 1;
-            }
+        int l=1;
+        int r=n;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            int ans=guess(m);
+            if(ans==0) return m;
+            else if(ans<0)  r=m-1;
+            else l=m+1;
         }
         return -1;
     }
